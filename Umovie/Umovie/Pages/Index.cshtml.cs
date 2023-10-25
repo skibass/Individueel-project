fb74908c18b6ecf1743c0f2a5aad059558d13deb
thijn <508700@student.fontys.nl>
2023-10-25 13:42:49 +0200

    Iteratie

---
+++ Umovie/Umovie/Pages/Index.cshtml.cs
@@ -0,0 +1,23 @@
+﻿using DALL;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.RazorPages;
+
+namespace Umovie.Pages
+{
+    public class IndexModel : PageModel
+    {
+        public User_Repository User_Repository= new User_Repository();
+
+        private readonly ILogger<IndexModel> _logger;
+
+        public IndexModel(ILogger<IndexModel> logger)
+        {
+            _logger = logger;
+        }
+
+        public void OnGet()
+        {
+
+        }
+    }
+}
