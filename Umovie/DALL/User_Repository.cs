2e33734ce183213c4e2b6a958b05c7958b6386cb
thijn <508700@student.fontys.nl>
2023-10-25 15:03:32 +0200

    Database connectie en repositories aangemaakt

---
+++ Umovie/DALL/User_Repository.cs
@@ -0,0 +1,43 @@
+﻿using Models;
+using MySql.Data.MySqlClient;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DALL
+{
+    public class User_Repository
+    {
+        MySqlConnection db = new MySqlConnection("Server=127.0.0.1;Database=umovie;Uid=root;Pwd=;");
+
+        public List<User> GetUsers()
+        {
+            List<User> users = new List<User>();
+
+            db.Open();
+
+            MySqlCommand accountQuery = new MySqlCommand("SELECT * from users", db);
+
+            accountQuery.ExecuteNonQuery();
+
+            MySqlDataReader readAccounts = accountQuery.ExecuteReader();
+
+            while (readAccounts.Read())
+            {
+                User user = new User();
+
+                user.Id = (int)readAccounts["user_id"];
+                user.Name = (string)readAccounts["user_name"];
+                user.Email = (string)readAccounts["user_email"];
+                user.RoleId = (int)readAccounts["role_id"];
+               
+                users.Add(user);
+            }
+            db.Close();
+
+            return users;
+        }
+    }
+}
---
+++ Umovie/Models/Movie.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Models
+{
+    public class Movie
+    {
+        public int Id { get; set; }
+        public string Name { get; set; }
+        public string Description { get; set; }
+        public string Director { get; set; }
+        public string Language { get; set; }
+        public string Release_Date { get; set; }
+    }
+}
