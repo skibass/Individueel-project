633733e3e4a315032c181df9108900dd40dbc69d
Thijn Senden <508700@student.fontys.nl>
2024-01-09 22:13:08 +0100

    Added visualization

--- VPT/VPT/Program.cs
+++ VPT/VPT/Program.cs
@@ -1,4 +1,26 @@
 ﻿using VptLibrary;
 Visitor visitor = new Visitor();
-EventSpace eventSpace = new EventSpace();   
-Console.WriteLine(visitor.Age + " " + visitor.Name + " ");
+EventSpace eventSpace = new EventSpace();
+
+Console.WriteLine("");
+
+foreach (var part in eventSpace.Parts)
+{
+    Console.WriteLine("-------------------------------");
+    Console.WriteLine(part.Letter);
+
+	foreach (var row in part.Rows)
+	{
+        Console.WriteLine("");
+        Console.WriteLine(row.EventRowName);
+        Console.WriteLine("");
+
+        foreach (var chair in row.Chairs)
+		{
+            Console.WriteLine(chair.EventChairName);
+        }
+    }
+    Console.WriteLine("");
+
+}
+
